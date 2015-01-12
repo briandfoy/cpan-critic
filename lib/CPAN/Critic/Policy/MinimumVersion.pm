b172e013b31b531ae2b103ca4ec4126802210624
brian d foy <brian.d.foy@gmail.com>
2015-01-12 04:04:16 -0500

    Implement the minimum version check

--- lib/CPAN/Critic/Policy/MinimumVersion.pm
+++ lib/CPAN/Critic/Policy/MinimumVersion.pm
@@ -1,21 +1,29 @@
 package CPAN::Critic::Policy::MinimumVersion;
 use v5.10;
 
-
 use strict;
 use warnings;
-
 
 use ReturnValue;
 
 sub run {
 	my( $class, @args ) = @_;
 
-	my( $value, $description, $tag ) = (
-		1,
-		'Null',
-		'null'
-		);
+	my $rv = CPAN::Critic::Util::MakefilePL->check_if_modulino();
+	return $rv unless $rv->is_success;
+
+	my $args = $rv->value;
+
+	no warnings 'uninitialized';
+
+	my( $value, $description, $tag ) = do {
+		if( ! exists $args->{MIN_PERL_VERSION} ) {
+			( 0, 'MIN_PERL_VERSION is in the data structure', 'found' );
+			}
+		else {
+			( $args->{MIN_PERL_VERSION}, 'The minimum version is there', '???' );
+			}
+		};
 
 	my $method = $value ? 'success' : 'error';
 
@@ -25,6 +33,7 @@ sub run {
 		tag         => $tag,
 		policy      => __PACKAGE__,
 		);
+	}
 	}
 
 
