9973b78e8953b36890205c9cc086ffd33b2b7f4d
brian d foy <brian.d.foy@gmail.com>
2015-01-13 06:00:12 -0500

    Implement the check for BugTracker

--- lib/CPAN/Critic/Policy/BugTracker.pm
+++ lib/CPAN/Critic/Policy/BugTracker.pm
@@ -1,7 +1,6 @@
 package CPAN::Critic::Policy::BugTracker;
 use v5.10;
 
-
 use strict;
 use warnings;
 
@@ -28,11 +27,35 @@ CPAN::Critic::Policy::BugTracker - The Makefile arguments specifies a bugtracker
 sub run {
 	my( $class, @args ) = @_;
 
-	my( $value, $description, $tag ) = (
-		1,
-		'Null',
-		'null'
-		);
+	my $rv = CPAN::Critic::Util::MakefilePL->check_if_modulino();
+	return $rv unless $rv->is_success;
+
+	my $args = $rv->value;
+
+	my $url = eval {
+		$args->{META_MERGE}{resources}{bugtracker}{web}
+		};
+
+	my( $value, $description, $tag ) = do {
+		if( ! exists $args->{META_MERGE} ) {
+			( 0, 'META_MERGE is in the data structure', 'found' );
+			}
+		elsif( ! exists $args->{META_MERGE}{resources} ) {
+			( 0, 'META_MERGE/resources is in the data structure', 'found' );
+			}
+		elsif( ! exists $args->{META_MERGE}{resources}{bugtracker} ) {
+			( 0, 'META_MERGE/resources/bugtracker is in the data structure', 'found' );
+			}
+		elsif( ! $args->{META_MERGE}{resources}{bugtracker}{web} ) {
+			( 0, 'META_MERGE/resources/bugtracker/web file is there', 'found' );
+			}
+		elsif( $url !~ m/issues/ ) {
+			( 0, "bugtracker has /issues, literally", '???' );
+			}
+		else {
+			( $url, 'The bugtracker is there', '???' );
+			}
+		};
 
 	my $method = $value ? 'success' : 'error';
 
