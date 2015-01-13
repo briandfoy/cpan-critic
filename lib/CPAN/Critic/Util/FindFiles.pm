20bc20fb9fa37aca63ddc135e8026f546c427876
brian d foy <brian.d.foy@gmail.com>
2015-01-13 04:53:57 -0500

    Utility functions to find files

---
+++ lib/CPAN/Critic/Util/FindFiles.pm
@@ -0,0 +1,63 @@
+package CPAN::Critic::Util::FindFiles;
+use strict;
+use warnings;
+
+use File::Find qw(find);
+use File::Spec::Functions qw(canonpath);
+
+=encoding utf8
+
+=head1 NAME
+
+CPAN::Critic::Util::FindFiles - Find files easily
+
+=head1 SYNOPSIS
+
+	use CPAN::Critic;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub find_by_name {
+	my( $class, @names ) = @_;
+	my %names = map { $_, 1 } @names;
+	my( $policy ) = ( caller(1) )[0];
+
+	my @files;
+	my $wanted = sub { 
+		push @files, canonpath( $File::Find::name ) if exists $names{$_} 
+		};
+
+	find( $wanted, '.' );
+
+	ReturnValue->success(
+		value => \@files,
+		);
+	}
+
+sub find_by_extension {
+	my( $class, @extensions ) = @_;
+	my %extensions = map { $_, 1 } @extensions;
+	my( $policy ) = ( caller(1) )[0];
+
+	my @files;
+	my $wanted = sub {
+		no warnings 'uninitialized';
+		my( $extension ) = /\.([^.]+)\z/;
+		push @files, canonpath( $File::Find::name ) 
+			if exists $extensions{$extension}; 
+		};
+
+	find( $wanted, '.' );
+
+	ReturnValue->success(
+		value => \@files,
+		);
+	}
+
+1;
