8bf560883a92e68dbdbeb7fa4589c807bc8ec99c
brian d foy <brian.d.foy@gmail.com>
2015-01-14 06:43:33 -0500

    Another monkey fix for Perl::MinimumVersion::Fast

--- lib/CPAN/Critic/Policy/ActualMinimumVersion.pm
+++ lib/CPAN/Critic/Policy/ActualMinimumVersion.pm
@@ -25,6 +25,8 @@ CPAN::Critic::Policy::ActualMinimumVersion - Check that the declared minimum ver
 
 =cut
 
+{
+no warnings qw(redefine);
 use List::Util qw(max);
 sub Perl::MinimumVersion::Fast::_build_minimum_explicit_version {
     my ($self, $tokens) = @_;
@@ -33,7 +35,7 @@ sub Perl::MinimumVersion::Fast::_build_minimum_explicit_version {
     my $explicit_version;
     for my $i (0..@tokens-1) {
         if ($tokens[$i]->{name} eq 'UseDecl' || $tokens[$i]->{name} eq 'RequireDecl') {
-            if (@$tokens >= $i+1) {
+			if (@tokens >= $i+1) {
                 my $next_token = $tokens[$i+1];
                 if ($next_token->{name} eq 'Double' || $next_token->{name} eq 'VersionString' ) {
                     $explicit_version = max($explicit_version // 0, version->new($next_token->{data}));
@@ -43,56 +45,67 @@ sub Perl::MinimumVersion::Fast::_build_minimum_explicit_version {
     }
     return $explicit_version;
 }
-
+}
 
 sub run {
 	my( $class, @args ) = @_;
 
-	my $rv = CPAN::Critic::Util::MakefilePL->check_if_modulino();
+	my $rv = CPAN::Critic::Util::MakefilePL->get_args;
 	return $rv unless $rv->is_success;
 
 	my $args = $rv->value;
 
 	my $modules = CPAN::Critic::Util::FindFiles->get_module_files->value;
+
+	my @results;
 	
-	foreach my $module ( @$modules ) {
+	foreach my $module ( sort @$modules ) {
 		no warnings 'uninitialized';
 		my $p = Perl::MinimumVersion::Fast->new( $module );
 		my $syntax_v   = $p->minimum_syntax_version;
-		my $declared_v = $p->minimum_explicit_version || version->new( '5.008' );
+		my $declared_v = $p->minimum_explicit_version;
+		say "$module -> $declared_v";
+		my $declared_v_default = $declared_v || version->new( '5.008' );
 
-		if( $syntax_v > $declared_v ) {
-			print "$module declared version is too low! Declared: $declared_v < $syntax_v\n";
+		if( ! defined $declared_v ) {
+			push @results, ReturnValue->error(
+				value            => 0,
+				description      => "$module has no declared minimum perl version",
+				syntax_version   => $syntax_v,
+				);
+			}
+ 
+		if( $declared_v && $syntax_v > $declared_v_default ) {
+			push @results, ReturnValue->error(
+				value            => 0,
+				description      => "$module declared perl version is too low! Declared: $declared_v < $syntax_v\n",
+				declared_version => $declared_v,
+				syntax_version   => $syntax_v,
+				);
 			}
 
-
+		unless( exists $args->{MIN_PERL_VERSION} ) {
+			push @results, ReturnValue->error(
+				value            => 0,
+				description      => "The build file doesn't specify MIN_PERL_VERSION",
+				);
+			}
+		elsif( version->new( $args->{MIN_PERL_VERSION} ) < $syntax_v ) {
+			push @results, ReturnValue->error(
+				value            => 0,
+				description      => "The build file MIN_PERL_VERSION is less than the syntax perl version",
+				declared_version => $args->{MIN_PERL_VERSION},
+				syntax_version   => $syntax_v,
+				);
+			}
 		}
 
-=pod
-
-	no warnings 'uninitialized';
-
-	my( $value, $description, $tag ) = do {
-		if( ! exists $args->{MIN_PERL_VERSION} ) {
-			( 0, 'MIN_PERL_VERSION is in the data structure', 'found' );
-			}
-		else {
-			( $args->{MIN_PERL_VERSION}, 'The minimum version is there', '???' );
-			}
-		};
-
-	my $method = $value ? 'success' : 'error';
+	my $method = @results ? 'error' : 'success';
 
 	ReturnValue->$method(
-		value       => $value,
-		description => $description,
-		tag         => $tag,
-		policy      => __PACKAGE__,
+		value  => \@results,
+		policy => __PACKAGE__,
 		);
-
-=cut
-
-	ReturnValue->success( value => 1 );
 	}
 
 
