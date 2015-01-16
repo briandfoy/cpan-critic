d1cefd4fdad21da3c6f2150d64e6a2e3587f3dd8
brian d foy <brian.d.foy@gmail.com>
2015-01-16 02:38:34 -0500

    Implement the check to compare actual prereqs to declared ones

--- lib/CPAN/Critic/Policy/ExplicitModulesInPrereqs.pm
+++ lib/CPAN/Critic/Policy/ExplicitModulesInPrereqs.pm
@@ -5,6 +5,9 @@ use strict;
 use warnings;
 
 use ReturnValue;
+use CPAN::Critic::Util::Lexer;
+use Data::Dumper;
+use List::Util qw(max);
 
 =encoding utf8
 
@@ -24,21 +27,68 @@ CPAN::Critic::Policy::ExplicitModulesInPrereqs - Check that all used modules sho
 
 =cut
 
+my %Ignores = map { $_, 1 } qw(
+	strict warnings vars subs
+	feature
+	);
+
 sub run {
 	my( $class, @args ) = @_;
 
-	my( $value, $description, $tag ) = (
-		1,
-		'Null',
-		'null'
-		);
+	my $files = CPAN::Critic::Util::FindFiles->get_module_files->value;
+	
+	my %found;
+	foreach my $file ( @$files ) {
+		my $namespaces = CPAN::Critic::Util::Lexer->get_namespaces( $file )->value;
+		foreach my $elem ( @$namespaces ) {
+			next unless defined $elem->[0];
+			my( $namespace, $version ) = @$elem;
+			
+			my $previous_max = $found{ $namespace } // 0;
+			
+			my $max_version = max( $previous_max, version->new( $version ) );
+			$found{ $namespace } = $max_version;
+			}
+		}
 
-	my $method = $value ? 'success' : 'error';
+	delete @found{ keys %Ignores };
+
+	my $rv = CPAN::Critic::Util::MakefilePL->get_args;
+	return $rv unless $rv->is_success;
+	my $args = $rv->value;
+
+	my $prereqs = $args->{PREREQ_PM};
+	foreach my $key ( sort keys %$prereqs ) {
+		$prereqs->{$key} = version->new( $prereqs->{$key} );
+		}
+
+	my @problems;
+	foreach my $key ( keys %found ) {
+		my $version = $found{$key} // 0;
+		
+		if( ! exists $prereqs->{$key} ) {
+			push @problems, ReturnValue->error(
+				value       => 0,
+				description => "Missing module in prereqs ($key)",
+				namespace   => $key,
+				); 
+			}
+		elsif( $prereqs->{$key} < $found{$key} ) {
+			push @problems, ReturnValue->error(
+				value       => 0,
+				description => "Prereq version ($prereqs->{$key}) of module ($key) is less than declared version ($found{$key})",
+				namespace   => $key,
+				declared_version => $prereqs->{$key},
+				required_version => $found{$key},
+				); 
+			}
+		}
+
+	my $method = @problems ? 'error' : 'success';
 
 	ReturnValue->$method(
-		value       => $value,
-		description => $description,
-		tag         => $tag,
+		value       => \@problems,
+		description => 'Required modules match declared prereqs',
 		policy      => __PACKAGE__,
 		);
 	}
