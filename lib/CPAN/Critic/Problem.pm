924305efdfe337f233b6643cf70866be24c2b2cf
brian d foy <brian.d.foy@gmail.com>
2015-01-16 10:04:42 -0500

    Objects to represent a problem

---
+++ lib/CPAN/Critic/Problem.pm
@@ -0,0 +1,68 @@
+package CPAN::Critic::Problem;
+use 5.010;
+use parent qw(Hash::AsObject);
+
+
+use CPAN::Critic::Basics;
+
+=encoding utf8
+
+=head1 NAME
+
+CPAN::Critic::Problem - represents something you need to fix
+
+=head1 SYNOPSIS
+
+	use CPAN::Critic;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+
+sub new {
+	my( $class, %args ) = @_;
+	
+	$args{policy} //= (caller(1))[0];
+
+	bless \%args, $class;
+	}
+
+
+
+
+
+
+
+
+
+
+
+=back
+
+=head1 TO DO
+
+
+=head1 SOURCE AVAILABILITY
+
+This code is in Github:
+
+	http://github.com/briandfoy/cpan-critic
+
+=head1 AUTHOR
+
+brian d foy, C<< <bdfoy@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2014-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+
+You may redistribute this under the same terms as Perl itself.
+
+=cut
+
+1;
