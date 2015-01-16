eb72ad13f73256234615b062b07c78306d9bc94c
brian d foy <brian.d.foy@gmail.com>
2015-01-16 02:37:20 -0500

    Make a note about what a policy can expect

--- lib/CPAN/Critic/Policy.pm
+++ lib/CPAN/Critic/Policy.pm
@@ -13,6 +13,9 @@ CPAN::Critic::Policy - a base class for Policies
 
 =head1 DESCRIPTION
 
+Each policy is guaranteed to start in the top level of the distribution
+directory.
+
 =over 4
 
 =item new
